9d31991fd19bfd3fe483c00e0e6ebabce6aa075d
Moshiur Rahman <rifatmoshiur00@gmail.com>
2024-03-28 06:33:19 +0600

    banner added

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -2,9 +2,14 @@
 
 const Banner = () => {
     return (
-        <div style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)',
-        }} className="mt-0 bg-green-100 hover:opacity:25 h-[80vh] opacity-25">
-            <h1 className="text-5xl font-bold flex justify-center items-center h-[60%] ">I Grow By Helping People In Need</h1>
+        <div
+         className="mt-0  h-[80vh]  ">
+            <img src="https://iili.io/Jh5tk8u.png" className="relative w-full h-full opacity-20" alt="" />
+            <h1 className="text-5xl  font-bold  absolute top-48 left-[370px] ">I Grow By Helping People In Need</h1>
+            <div className="absolute top-80 left-[430px]">
+            <input className="w-[500px] p-3 rounded-lg border" type="text" placeholder="Search here..." name="" id="search" />
+            <button className="btn bg-[#FF444A] px-4 py-2 rounded-md text-white font-medium">Search</button>
+            </div>
         </div>
     );
 };
