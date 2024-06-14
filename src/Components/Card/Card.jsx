bf0f04b29b1c393cf5d8c672da74a55aa59573cd
Moshiur Rahman <rifatmoshiur00@gmail.com>
2024-06-14 10:35:58 +0600

    text,backgound color related problem fixed. Final Push!

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,21 +4,25 @@ import { Link } from "react-router-dom";
 
 
 const Card = ({ card }) => {
-    const { id,title,category, picture,text_button_bg,card_bg,category_bg } = card;
+    const { id, title, category, picture, text_button_bg, card_bg, category_bg } = card;
     return (
         <div>
             <Link to={`/card/${id}`}>
-            <div className={`rounded-lg ${card_bg}`}>
-                <figure><img src={picture} alt="Shoes" /></figure>
-                <div className="pl-5 py-4">
-                    <p className={`font-medium bg-[${category_bg}] px-2 py-1 rounded-md w-max text-[${text_button_bg}]`}>{category}</p>
-                    <h2 className={`text-xl font-semibold text-[${text_button_bg}]`}>
-                        {title}
-                    </h2>
+                <div style={{ backgroundColor: card_bg }} className={`rounded-lg `}>
+                    <figure><img src={picture} alt="Shoes" /></figure>
+                    <div className="pl-5 py-4">
+                        <div className={`font-medium  px-2 py-1 rounded-md w-max ]`} style={{
+                            backgroundColor: category_bg,
+                            color: text_button_bg
+                        }} >{category}</div>
+                        {/* <p className={`font-medium bg-[${category_bg}] px-2 py-1 rounded-md w-max text-[${text_button_bg}]`}>{category}</p> */}
+                        <h2 style={{ color: text_button_bg }} className={`text-xl font-semibold ]`}>
+                            {title}
+                        </h2>
+                    </div>
                 </div>
-            </div>
             </Link>
-            
+
         </div>
     );
 };
